b50c0a0b73bb2c47430967e0c6d512622e6baab9
Davit-77 <dhakobyan750@gmail.com>
2025-09-21 22:56:22 +0400

    Update main.js

--- main.js
+++ main.js
@@ -21,22 +21,38 @@ function toggleTask(index) {
 }
 
 
+function editTask(index) {
+    if (tasks[index].completed === true) return
+    const newText = prompt("Change task name", tasks[index].text)
+
+    if (newText && newText.trim() !== "") {
+        tasks[index].text = newText
+    }
+    renderTasks()
+
+}
+
+
+
 
 
 function renderTasks() {
     const taskslist = document.getElementById('taskslist')
     taskslist.innerHTML = ""
+    sortCompleted()
     tasks.forEach((task, index) => {
         const li = document.createElement('li')
         li.className = task.completed ? "completed" : ""
         li.innerHTML = `<span>${task.text}</span>
          <button class = "completedbtn" onClick ="toggleTask(${index})">✅</button>
-        <button class = "delete" onClick ="deleteTask(${index})">🗑️</button`
+        <button class = "delete" onClick ="deleteTask(${index})">🗑️</button>
+        <button onClick = "editTask(${index})">✎</button>`
 
         taskslist.appendChild(li)
     }
     )
     taskCount()
+    saveTasks()
 
 
 }
@@ -54,6 +70,36 @@ function taskCount() {
 
 }
 
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function loadTasks() {
+    const saved = localStorage.getItem('tasks')
+    if (saved) {
+        const parsed = JSON.parse(saved)
+        tasks.length = 0
+        parsed.forEach(t => tasks.push(t))
+        renderTasks()
+    }
+}
+
+function sortCompleted() {
+    const notCompleted = []
+    const completed = []
+
+    tasks.map(task => {
+        if (task.completed) {
+            completed.push(task)
+        } else {
+            notCompleted.push(task)
+        }
+    })
+    tasks.length = 0
+    tasks.push(...notCompleted, ...completed)
+}
+
+
 
 document.getElementById('taskbtn').addEventListener('click', () => {
     const input = document.getElementById('taskinput')
@@ -66,3 +112,4 @@ document.getElementById("deleteall").addEventListener("click", () => {
     tasks.length = 0;
     renderTasks()
 })
+window.addEventListener('load', loadTasks)
